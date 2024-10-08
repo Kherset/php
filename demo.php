e8382d3af2ab479a56dda2809166ec0adde2860b
jeremy twardowski <kherset@protonmail.com>
2024-10-08 21:14:35 +0200

    exercice 1 boucle foreach

--- demo.php
+++ demo.php
@@ -1,3 +1,32 @@
 <?php
+// $chiffre = null;
+// $action = (int)readline('Entrez une heure : ');
 
+// while ($chiffre !== 10) {
+//   $chiffre = (int)readline('Entrez une heure : ');
+// }
+
+// echo 'Bravo vous avez gagné !'
+
+// for ($i = 0; $i <= 100; $i += 2) {
+//   echo "- $i \n";
+// }
+
+// $notes = [10, 15, 16];
+
+// foreach ($notes as $note) {
+//   echo "- $note \n";
+// }
+
+$eleves = [
+  'cm2' => ['Jean', 'Paul', 'Arthur', 'Lucas'],
+  'cm1' => ['Aksel', 'Elio']
+];
+
+foreach ($eleves as $class => $listEleves) {
+  echo "Dans la classe de $class il y a \n";
+  foreach ($listEleves as $Eleve) {
+    echo "- $Eleve \n";
+  }
+};
 ?>
