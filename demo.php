b4d55d80e55b91ef08044ca240084a7a21171ece
jeremy twardowski <kherset@protonmail.com>
2024-10-08 20:40:49 +0200

    Les boucles

--- demo.php
+++ demo.php
@@ -1,17 +1,3 @@
 <?php
-$action = (int)readline('Entrez votre action : (1: Attaquer, 2: Defendre, 3: Passer son tour');
 
-switch ($action) {
-  case 1:
-    echo "J'attaque";
-    break;
-  case 2:
-    echo "Je defends";
-    break;
-  case 3:
-    echo "Je passe mon tour";
-    break;
-    default:
-  echo "commande inconnue";
-}
 ?>
