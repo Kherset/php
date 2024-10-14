c2d18dc0a520187d3c59160d8edee92d6ea83405
jeremy twardowski <kherset@protonmail.com>
2024-10-14 22:49:17 +0200

    exercice 3 fonctions

--- demo.php
+++ demo.php
@@ -1,10 +1,14 @@
 <?php
-$word = readline("tapez un mot\n");
-$reverseword = strrev($word);
+// $word = readline("tapez un mot\n");
+// $reverseword = strrev($word);
 
-if (strtolower($word) == strtolower($reverseword)) {
-  echo "C'est un pallindrome";
-} else {
-  echo "Ce n'est un pallindrome";
-}
+// if (strtolower($word) == strtolower($reverseword)) {
+//   echo "C'est un pallindrome";
+// } else {
+//   echo "Ce n'est un pallindrome";
+// }
+
+$notes = [10, 20, 10];
+$moyenne = array_sum($notes) / count($notes);
+echo round($moyenne);
 ?>
