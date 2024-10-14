31b486881212c569dc5df3c1bd7ad61a2df06479
jeremy twardowski <kherset@protonmail.com>
2024-10-14 22:38:12 +0200

    exercice 1 et 2 fonctions

--- demo.php
+++ demo.php
@@ -1,3 +1,10 @@
 <?php
+$word = readline("tapez un mot\n");
+$reverseword = strrev($word);
 
+if (strtolower($word) == strtolower($reverseword)) {
+  echo "C'est un pallindrome";
+} else {
+  echo "Ce n'est un pallindrome";
+}
 ?>
