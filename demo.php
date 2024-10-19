9b475ed22207b370cd630508b1db0780f93d4632
jeremy twardowski <kherset@protonmail.com>
2024-10-19 22:23:54 +0200

    Creer une fonction exercice 1 fini

--- demo.php
+++ demo.php
@@ -1,3 +1,28 @@
 <?php
+// $nom = 'Doe';
 
+//   function bonjour ($prenom = null) {
+//     global $nom;
+//     if ($prenom == null) {
+//       return "Bonjour\n";
+//     }
+//     return 'Bonjour ' . $prenom . " " . "$nom" . "\n";
+//   }
+
+// echo bonjour('Jean');
+
+
+function repondre_oui_non($phrase) {
+  $answer = readline("$phrase si oui tapez 'o', sinon tapez 'n' \n");
+  if ($answer === 'o') {
+    return true;
+  } if ($answer === 'n') {
+    return false;
+  } else {
+    return repondre_oui_non('Voulez vous continuer ?');
+  }
+}
+
+$resultat = repondre_oui_non('Voulez vous continuer ?');
+var_dump($resultat);
 ?>
